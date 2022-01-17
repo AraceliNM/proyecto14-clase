7b53fa52b586ecfd7967cfd5d5ec2d8f46304eb8
Araceli <araceli292.nm@gmail.com>
2022-01-17 16:01:49 +0000

    Fin Paso 48

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -6,13 +6,20 @@ use Livewire\Component;
 
 class AddCartItem extends Component
 {
+    public $product;
+    public $quantity;
     public $qty = 1;
+
+    public function mount()
+    {
+        $this->quantity = $this->product->quantity;
+    }
 
     public function decrement()
     {
         $this->qty--;
     }
-    
+
     public function increment()
     {
         $this->qty++;
