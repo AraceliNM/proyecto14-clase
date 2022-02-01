d78ebb46915f6819421b46cd03af79ea89ed5a86
Araceli <araceli292.nm@gmail.com>
2022-02-01 14:40:48 +0000

    Fin Paso 109

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Livewire\CreateOrder;
+use App\Http\Livewire\PaymentOrder;
+use App\Http\Livewire\ShoppingCart;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,16 +21,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
 Route::get('/', WelcomeController::class);
 
 Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
 
-Route::get('/deletecart', function () {
-    \Cart::destroy();
-});
+Route::get('search', SearchController::class)->name('search');
+
+Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
+
+Route::get('orders/create', CreateOrder::class)->middleware('auth')->name('orders.create');
+
+Route::get('orders/{order}/payment', PaymentOrder::class)->name('orders.payment');
+
+Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
