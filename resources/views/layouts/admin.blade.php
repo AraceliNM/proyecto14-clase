706c7ecf103b038f5a34c9358454d3a1954a322d
Araceli <araceli292.nm@gmail.com>
2022-02-09 15:22:26 +0000

    Fin Paso 144

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,6 +15,7 @@
         <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/glider-js/glider.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/flex-slider/flexslider.css') }}">
+        <link rel="stylesheet" href="{{ asset('vendor/sweetalert2/dist/sweetalert2.css') }}">
 
         @livewireStyles
 
@@ -24,6 +25,7 @@
         <script src="{{ asset('vendor/glider-js/glider.min.js') }}"></script>
         <script src="{{ asset('vendor/flex-slider/node_modules/jquery/dist/jquery.js') }}"></script>
         <script src="{{ asset('vendor/flex-slider/jquery.flexslider-min.js') }}"></script>
+        <script src="{{ asset('vendor/sweetalert2/dist/sweetalert2.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
