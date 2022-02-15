7bf799b99108f9f9475db054ad276286495fb65a
Araceli <araceli292.nm@gmail.com>
2022-02-15 16:04:07 +0000

    Fin Paso 175

--- resources/views/livewire/admin/department-component.blade.php
+++ resources/views/livewire/admin/department-component.blade.php
@@ -53,7 +53,7 @@
                 @foreach ($departments as $department)
                     <tr>
                         <td class="py-2">
-                            <a href="" class="uppercase underline hover:text-blue-600">
+                            <a href="{{route('admin.departments.show', $department)}}" class="uppercase underline hover:text-blue-600">
                                 {{$department->name}}
                             </a>
                         </td>
@@ -95,7 +95,7 @@
             </x-jet-danger-button>
         </x-slot>
     </x-jet-dialog-modal>
-    
+
     @push('scripts')
         <script>
             Livewire.on('deleteDepartment', departmentId => {
